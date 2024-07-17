35dafd7ca0285ca11255ef80da8b27b4849d040d
Gynt <gynt@users.noreply.github.com>
2024-07-17 13:49:39 +0200

    Update discovery.lua

--- discovery.lua
+++ discovery.lua
@@ -304,14 +304,13 @@ local function discoverMapFiles_hook(this, ptr_pattern)
     
   else
     log(WARNING, string.format("Unhandled pattern: %s", pattern))
+    -- Insert the default requested pattern
+    table.insert(patterns, pattern)
   end
-  
-  log(VERBOSE, string.format("discoverMapFiles: handler: %s", tostring(dh)))
-  log(VERBOSE, string.format("discoverMapFiles: handler: func: %s", tostring(dh.discoverForPattern)))
   
   for _, p in ipairs(patterns) do
     if dh:discoverForPattern(p) == false then
-      log(WARNING, "too many map files are being loaded!")
+      log(WARNING, "Too many map files (>499) are being loaded! List of maps will be truncated.")
       break
     end
   end
